a82ce18a7919048076db873abb680410ee6fbe8e
Yuma Ohgami <yuma@ohgami.jp>
2020-05-14 06:51:15 +0900

    [WIP] apps: add lifegame

---
+++ apps/lifegame/main.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+set -uex
+# set -ue
+
+. include/gb.sh
+. include/map.sh
+
+VARS_BASE=$GB_WRAM1_BASE	# make_bin()で更新する
+
+vars() {
+	# 汎用フラグ変数
+	echo -en '\x00'	# 全て0
+}
+
+init_glider() {
+	lr35902_set_reg regA 09
+	lr35902_set_reg regD 03
+	lr35902_set_reg regE 03
+	lr35902_call $a_lay_tile_at_wtcoord
+}
+
+main() {
+	local flg_bitnum_inited=0
+
+	# push
+	lr35902_push_reg regAF
+
+	# フラグ変数の初期化済みフラグチェック
+	lr35902_copy_to_regA_from_addr $VARS_BASE
+	lr35902_test_bitN_of_reg $flg_bitnum_inited regA
+
+	# フラグがセットされていたら(初期化済みだったら)、
+	# 初期化処理をスキップ
+	(
+		# 初期化処理
+		init_glider
+
+		# 初期化済みフラグをセット
+		lr35902_copy_to_regA_from_addr $VARS_BASE
+		lr35902_set_bitN_of_reg $flg_bitnum_inited regA
+		lr35902_copy_to_addr_from_regA $VARS_BASE
+	) >main.1.o
+	local sz_1=$(stat -c '%s' main.1.o)
+	lr35902_rel_jump_with_cond NZ $(two_digits_d $sz_1)
+	cat main.1.o
+
+	# (
+	# 	# 定常処理
+	# )
+
+	# pop & return
+	lr35902_pop_reg regAF
+	lr35902_return
+}
+
+make_bin() {
+	# vars.o生成
+	vars >vars.o
+
+	# main.oのサイズ確認
+	main >main.o
+	local sz_main=$(stat -c '%s' main.o)
+	local sz_main16=$(to16 $sz_main)
+
+	# 変数領域ベースアドレス算出
+	VARS_BASE=$(calc16 "$VARS_BASE + $sz_main16")
+
+	# main.o再生成
+	main >main.o
+
+	# 結合
+	cat main.o vars.o
+}
+
+make_bin
