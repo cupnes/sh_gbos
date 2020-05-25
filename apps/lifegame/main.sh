80a7f65cb922da17c823bf99440ad87f46c07d34
Yuma Ohgami <yuma@ohgami.jp>
2020-05-25 21:57:35 +0900

    [WIP] apps/lifegame: edit

--- apps/lifegame/main.sh
+++ apps/lifegame/main.sh
@@ -244,73 +244,6 @@ f_tdq_enq() {
 	lr35902_return
 }
 
-funcs() {
-	local fsz
-
-	# 指定したセルの生死を取得
-	a_get_cell_is_alive=$APP_FUNCS_BASE
-	echo -e "a_get_cell_is_alive=$a_get_cell_is_alive" >>$map_file
-	f_get_cell_is_alive
-
-	# tdqへエントリを追加する
-	f_get_cell_is_alive >f_get_cell_is_alive.o
-	fsz=$(to16 $(stat -c '%s' f_get_cell_is_alive.o))
-	a_tdq_enq=$(four_digits $(calc16 "${a_get_cell_is_alive}+${fsz}"))
-	echo -e "a_tdq_enq=$a_tdq_enq" >>$map_file
-	f_tdq_enq
-}
-# 変数設定のために空実行
-funcs >/dev/null
-rm -f $map_file
-
-init_glider() {
-	local base_x=$GBOS_WIN_DRAWABLE_BASE_XT
-	local base_y=$GBOS_WIN_DRAWABLE_BASE_YT
-
-	lr35902_set_reg regB $GBOS_TILE_NUM_BLACK
-
-	lr35902_set_reg regD $base_y
-	lr35902_set_reg regE $(calc16_2 "$base_x+1")
-	lr35902_call $a_tcoord_to_addr
-	lr35902_copy_to_from regD regH
-	lr35902_copy_to_from regE regL
-	lr35902_call $a_tdq_enq
-
-	lr35902_set_reg regD $(calc16_2 "$base_y+1")
-	lr35902_set_reg regE $(calc16_2 "$base_x+2")
-	lr35902_call $a_tcoord_to_addr
-	lr35902_copy_to_from regD regH
-	lr35902_copy_to_from regE regL
-	lr35902_call $a_tdq_enq
-
-	lr35902_set_reg regD $(calc16_2 "$base_y+2")
-	lr35902_set_reg regE $base_x
-	lr35902_call $a_tcoord_to_addr
-	lr35902_copy_to_from regD regH
-	lr35902_copy_to_from regE regL
-	lr35902_call $a_tdq_enq
-
-	lr35902_set_reg regD $(calc16_2 "$base_y+2")
-	lr35902_set_reg regE $(calc16_2 "$base_x+1")
-	lr35902_call $a_tcoord_to_addr
-	lr35902_copy_to_from regD regH
-	lr35902_copy_to_from regE regL
-	lr35902_call $a_tdq_enq
-
-	lr35902_set_reg regD $(calc16_2 "$base_y+2")
-	lr35902_set_reg regE $(calc16_2 "$base_x+2")
-	lr35902_call $a_tcoord_to_addr
-	lr35902_copy_to_from regD regH
-	lr35902_copy_to_from regE regL
-	lr35902_call $a_tdq_enq
-
-	# var_draw_cycを1にするエントリを積む
-	lr35902_set_reg regB 01
-	lr35902_set_reg regD $(echo $var_draw_cyc | cut -c1-2)
-	lr35902_set_reg regE $(echo $var_draw_cyc | cut -c3-4)
-	lr35902_call $a_tdq_enq
-}
-
 # 指定したセルの8近傍の生きているセルの数を返す
 # in : regD  - タイル座標Y
 #      regE  - タイル座標X
@@ -373,7 +306,13 @@ get_num_live_cells_8_neighbors() {
 # 指定した座標のセルを更新
 # in : regD  - タイル座標Y
 #      regE  - タイル座標X
-update_cell() {
+f_update_cell() {
+	# push
+	lr35902_push_reg regAF
+	lr35902_push_reg regBC
+	lr35902_push_reg regDE
+	lr35902_push_reg regHL
+
 	# 8近傍の生きているセルの数をCへ取得
 	get_num_live_cells_8_neighbors
 	lr35902_copy_to_from regC regA
@@ -446,7 +385,90 @@ update_cell() {
 	lr35902_rel_jump_with_cond Z $(two_digits_d $sz_2)
 	cat update_cell.2.o
 	cat update_cell.1.o
+
+	# pop & return
+	lr35902_pop_reg regHL
+	lr35902_pop_reg regDE
+	lr35902_pop_reg regBC
+	lr35902_pop_reg regAF
+	lr35902_return
 }
+
+funcs() {
+	local fsz
+
+	# 指定したセルの生死を取得
+	a_get_cell_is_alive=$APP_FUNCS_BASE
+	echo -e "a_get_cell_is_alive=$a_get_cell_is_alive" >>$map_file
+	f_get_cell_is_alive
+
+	# tdqへエントリを追加する
+	f_get_cell_is_alive >f_get_cell_is_alive.o
+	fsz=$(to16 $(stat -c '%s' f_get_cell_is_alive.o))
+	a_tdq_enq=$(four_digits $(calc16 "${a_get_cell_is_alive}+${fsz}"))
+	echo -e "a_tdq_enq=$a_tdq_enq" >>$map_file
+	f_tdq_enq
+
+	# 指定した座標のセルを更新
+	f_tdq_enq >f_tdq_enq.o
+	fsz=$(to16 $(stat -c '%s' f_tdq_enq.o))
+	a_update_cell=$(four_digits $(calc16 "${a_tdq_enq}+${fsz}"))
+	echo -e "a_update_cell=$a_update_cell" >>$map_file
+	f_update_cell
+}
+# 変数設定のために空実行
+funcs >/dev/null
+rm -f $map_file
+
+init_glider() {
+	local base_x=$GBOS_WIN_DRAWABLE_BASE_XT
+	local base_y=$GBOS_WIN_DRAWABLE_BASE_YT
+
+	lr35902_set_reg regB $GBOS_TILE_NUM_BLACK
+
+	lr35902_set_reg regD $base_y
+	lr35902_set_reg regE $(calc16_2 "$base_x+1")
+	lr35902_call $a_tcoord_to_addr
+	lr35902_copy_to_from regD regH
+	lr35902_copy_to_from regE regL
+	lr35902_call $a_tdq_enq
+
+	lr35902_set_reg regD $(calc16_2 "$base_y+1")
+	lr35902_set_reg regE $(calc16_2 "$base_x+2")
+	lr35902_call $a_tcoord_to_addr
+	lr35902_copy_to_from regD regH
+	lr35902_copy_to_from regE regL
+	lr35902_call $a_tdq_enq
+
+	lr35902_set_reg regD $(calc16_2 "$base_y+2")
+	lr35902_set_reg regE $base_x
+	lr35902_call $a_tcoord_to_addr
+	lr35902_copy_to_from regD regH
+	lr35902_copy_to_from regE regL
+	lr35902_call $a_tdq_enq
+
+	lr35902_set_reg regD $(calc16_2 "$base_y+2")
+	lr35902_set_reg regE $(calc16_2 "$base_x+1")
+	lr35902_call $a_tcoord_to_addr
+	lr35902_copy_to_from regD regH
+	lr35902_copy_to_from regE regL
+	lr35902_call $a_tdq_enq
+
+	lr35902_set_reg regD $(calc16_2 "$base_y+2")
+	lr35902_set_reg regE $(calc16_2 "$base_x+2")
+	lr35902_call $a_tcoord_to_addr
+	lr35902_copy_to_from regD regH
+	lr35902_copy_to_from regE regL
+	lr35902_call $a_tdq_enq
+
+	# var_draw_cycを1にするエントリを積む
+	lr35902_set_reg regB 01
+	lr35902_set_reg regD $(echo $var_draw_cyc | cut -c1-2)
+	lr35902_set_reg regE $(echo $var_draw_cyc | cut -c3-4)
+	lr35902_call $a_tdq_enq
+}
+
+
 
 main() {
 	local flg_bitnum_inited=0
@@ -485,9 +507,40 @@ main() {
 		cat main.3.o
 
 		# 更新処理
-		lr35902_set_reg regD 03
-		lr35902_set_reg regE 02
-		update_cell
+		# Y(regD)= 03 - 0f, X(regE)= 02 - 11 で update_cell() を呼び出す
+		lr35902_set_reg regD $GBOS_WIN_DRAWABLE_BASE_YT
+		(
+			lr35902_set_reg regE $GBOS_WIN_DRAWABLE_BASE_XT
+
+			(
+				lr35902_call $a_update_cell
+
+				lr35902_inc regE
+				lr35902_copy_to_from regA regE
+				lr35902_compare_regA_and $(calc16_2 "$GBOS_WIN_DRAWABLE_BASE_XT+$GBOS_WIN_DRAWABLE_WIDTH_T")
+			) >main.4.o
+			cat main.4.o
+			local sz_4=$(stat -c '%s' main.4.o)
+			lr35902_rel_jump_with_cond C $(two_comp_d $((sz_4 + 2)))
+
+			lr35902_inc regD
+			lr35902_copy_to_from regA regD
+			lr35902_compare_regA_and $(calc16_2 "$GBOS_WIN_DRAWABLE_BASE_YT+$GBOS_WIN_DRAWABLE_HEIGHT_T")
+		) >main.5.o
+		cat main.5.o
+		local sz_5=$(stat -c '%s' main.5.o)
+		lr35902_rel_jump_with_cond C $(two_comp_d $((sz_5 + 2)))
+
+		# var_draw_cycをインクリメントするエントリを積む
+		lr35902_copy_to_regA_from_addr $var_draw_cyc
+		lr35902_set_reg regD $(echo $var_draw_cyc | cut -c1-2)
+		lr35902_set_reg regE $(echo $var_draw_cyc | cut -c3-4)
+		lr35902_call $a_tdq_enq
+
+		# var_proc_cycをインクリメント
+		lr35902_copy_to_regA_from_addr $var_proc_cyc
+		lr35902_inc regA
+		lr35902_copy_to_addr_from_regA $var_proc_cyc
 	) >main.2.o
 
 	(
