7bb5098e3f01218e9587377d80177fd643288d9f
Yuma Ohgami <yuma@ohgami.jp>
2020-11-01 15:54:59 +0900

    [WIP] paint: edit

--- apps/wip/paint/main.sh
+++ apps/wip/paint/main.sh
@@ -21,21 +21,15 @@ APP_FUNCS_BASE=$(calc16 "$APP_VARS_BASE+$APP_VARS_SZ")
 map_file=map.sh
 rm -f $map_file
 
+# RAM
+RAM_BKUP_NEXT_ADDR_TH=a000
+RAM_BKUP_NEXT_ADDR_BH=a001
+
 vars() {
 	# 汎用フラグ変数
 	var_general_flgs=$APP_VARS_BASE
 	echo -e "var_general_flgs=$var_general_flgs" >>$map_file
 	echo -en '\x00'	# 全て0
-
-	# バックアップアドレス(下位8ビット)
-	var_bkup_bh=$(calc16 "$var_general_flgs+1")
-	echo -e "var_bkup_bh=$var_bkup_bh" >>$map_file
-	echo -en '\x02'
-
-	# バックアップアドレス(上位8ビット)
-	var_bkup_th=$(calc16 "$var_bkup_bh+1")
-	echo -e "var_bkup_th=$var_bkup_th" >>$map_file
-	echo -en '\xa0'
 }
 # 変数設定のために空実行
 vars >/dev/null
@@ -145,10 +139,25 @@ main() {
 			lr35902_set_reg regB $GBOS_TILE_NUM_BLACK
 			lr35902_call $a_enq_tdq
 
-			# TODO バックアップ
+			# バックアップ
 			## WALもどき
-			## var_bkup_{th,bh}が挿す場所にX,Y座標値を書いて
-			## var_bkup_{th,bh}を更新(+2バイトする)
+			## RAM_BKUP_NEXT_ADDR_{TH,BH}が指すRAMアドレスを
+			## regHLへ設定
+			lr35902_copy_to_regA_from_addr $RAM_BKUP_NEXT_ADDR_TH
+			lr35902_copy_to_from regH regA
+			lr35902_copy_to_regA_from_addr $RAM_BKUP_NEXT_ADDR_BH
+			lr35902_copy_to_from regL regA
+			## 先程描画したVRAMアドレスをregHLが指す先へ保存
+			## (regHLをインクリメントしながら)
+			lr35902_copy_to_from regA regD	# VRAMアドレス[15:8]
+			lr35902_copyinc_to_ptrHL_from_regA
+			lr35902_copy_to_from regA regE	# VRAMアドレス[7:0]
+			lr35902_copyinc_to_ptrHL_from_regA
+			## regHLをRAM_BKUP_NEXT_ADDR_{TH,BH}へ書き戻す
+			lr35902_copy_to_from regA regH
+			lr35902_copy_to_addr_from_regA $RAM_BKUP_NEXT_ADDR_TH
+			lr35902_copy_to_from regA regL
+			lr35902_copy_to_addr_from_regA $RAM_BKUP_NEXT_ADDR_BH
 
 			# リリース情報のBボタン(左クリック)のビットをクリア
 			lr35902_copy_to_from regA regC
@@ -159,16 +168,70 @@ main() {
 		lr35902_rel_jump_with_cond Z $(two_digits_d $sz_3)
 		cat main.3.o
 
-		# TODO セレクトボタン: バックアップをロード
+		# セレクトボタン: バックアップをロード
 		## カートリッジRAMのデータを元に画面描画する
-		## var_bkup_{th,bh}変数の内容もRAM[0:1]の内容で更新
 		## バックアップデータのフォーマット:
-		## A000h | var_bkup_bh   | var_bkup_th   |
-		## A002h | draw_log[0].x | draw_log[0].y |
-		## A004h | draw_log[1].x | draw_log[1].y |
-		## ※ var_bkup_{th,bh}は次にログを保存できる場所を挿す
+		## A000h | RAM_BKUP_NEXT_ADDR_TH | RAM_BKUP_NEXT_ADDR_BH |
+		## A002h | draw_log[0].th        | draw_log[0].bh        |
+		## A004h | draw_log[1].th        | draw_log[1].bh        |
+		## ※ RAM_BKUP_NEXT_ADDR_{TH,BH}は次にログを保存できる場所を指す
 		##    この例の場合、A003hまではデータが既に入っているので
-		##    var_bkup_th=A0h,var_bkup_bh=06hとなる
+		##    RAM_BKUP_NEXT_ADDR_TH=A0h,RAM_BKUP_NEXT_ADDR_BH=06hとなる
+		lr35902_test_bitN_of_reg $GBOS_SELECT_KEY_BITNUM regA
+		(
+			lr35902_set_reg regB $GBOS_TILE_NUM_BLACK
+
+			lr35902_set_reg regH a0
+			lr35902_set_reg regL 02
+
+			# 繰り返す対象の処理
+			# (regHLの指す先をtdqへエンキュー)
+			(
+				lr35902_copyinc_to_regA_from_ptrHL
+				lr35902_copy_to_from regD regA
+				lr35902_copyinc_to_regA_from_ptrHL
+				lr35902_copy_to_from regE regA
+				lr35902_call $a_enq_tdq
+			) >main.5.o
+			local sz_5=$(stat -c '%s' main.5.o)
+			## 条件判定処理先頭までジャンプしている命令の分を足す
+			local sz_5_rel_jump=$((sz_5 + 2))
+
+			# regH < RAM_BKUP_NEXT_ADDR_TH の間繰り返す
+			(
+				lr35902_copy_to_regA_from_addr $RAM_BKUP_NEXT_ADDR_TH
+				lr35902_copy_to_from regC regA
+				lr35902_copy_to_from regA regH
+				lr35902_compare_regA_and regC
+				## regH(regA) >= RAM_BKUP_NEXT_ADDR_TH(regC)
+				## だったらジャンプして飛ばす
+				lr35902_rel_jump_with_cond NC $(two_digits_d $sz_5_rel_jump)
+				cat main.5.o
+			) >main.7.o
+			cat main.7.o
+			## 条件判定処理先頭までジャンプ
+			local sz_7=$(stat -c '%s' main.7.o)
+			lr35902_rel_jump $(two_comp_d $((sz_7 + 2)))
+
+			# regL < RAM_BKUP_NEXT_ADDR_BH の間繰り返す
+			(
+				lr35902_copy_to_regA_from_addr $RAM_BKUP_NEXT_ADDR_BH
+				lr35902_copy_to_from regC regA
+				lr35902_copy_to_from regA regL
+				lr35902_compare_regA_and regC
+				## regL(regA) >= RAM_BKUP_NEXT_ADDR_BH(regC)
+				## だったらジャンプして飛ばす
+				lr35902_rel_jump_with_cond NC $(two_digits_d $sz_5_rel_jump)
+				cat main.5.o
+			) >main.8.o
+			cat main.8.o
+			## 条件判定処理先頭までジャンプ
+			local sz_8=$(stat -c '%s' main.8.o)
+			lr35902_rel_jump $(two_comp_d $((sz_8 + 2)))
+		) >main.4.o
+		local sz_4=$(stat -c '%s' main.4.o)
+		lr35902_rel_jump_with_cond Z $(two_digits_d $sz_4)
+		cat main.4.o
 	) >main.2.o
 
 	(
@@ -179,6 +242,23 @@ main() {
 		# カートリッジ搭載RAMの有効化
 		lr35902_set_reg regA $GB_MBC_RAM_EN_VAL
 		lr35902_copy_to_addr_from_regA $GB_MBC_RAM_EN_ADDR
+
+		# RAM_BKUP_NEXT_ADDR_TH にバックアップ領域のアドレス
+		# の上位8ビット(A0h 〜 AFh)が書かれているか確認
+		lr35902_copy_to_regA_from_addr $RAM_BKUP_NEXT_ADDR_TH
+		lr35902_and_to_regA f0
+		lr35902_compare_regA_and a0
+		(
+			# 書かれていなかったら
+			# RAM_BKUP_NEXT_ADDR_{TH,BH}を初期化
+			lr35902_set_reg regA a0
+			lr35902_copy_to_addr_from_regA $RAM_BKUP_NEXT_ADDR_TH
+			lr35902_set_reg regA 02
+			lr35902_copy_to_addr_from_regA $RAM_BKUP_NEXT_ADDR_TH
+		) >main.9.o
+		local sz_9=$(stat -c '%s' main.9.o)
+		lr35902_rel_jump_with_cond Z $(two_digits_d $sz_9)
+		cat main.9.o
 
 		# 初期化済みフラグをセット
 		lr35902_copy_to_regA_from_addr $APP_VARS_BASE
