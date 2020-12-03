0fe639e5c65b50f46f03e09dd585afaf93f54f40
Yuma Ohgami <yuma@ohgami.jp>
2020-12-04 01:13:54 +0900

    tools: fix make_ram0_files.sh

--- tools/make_ram0_files.sh
+++ tools/make_ram0_files.sh
@@ -19,9 +19,9 @@ for i in $(seq -w 05 08); do
 	dd if=/dev/zero of=0${i}0.exe bs=1 count=192
 done
 
-# 3行目：14画面分(672バイト)の空のEXE4つ
+# 3行目：13画面分(624バイト)の空のEXE4つ
 for i in $(seq -w 09 12); do
-	dd if=/dev/zero of=0${i}0.exe bs=1 count=672
+	dd if=/dev/zero of=0${i}0.exe bs=1 count=624
 done
 
 # 4行目：空のEXEが2KB2つ、そして空の1画面分(208バイト)TXT2つ
